7704e6ac8350a8fb1daa49d47532b98e28fcf9bf
timfallmk <tim@midokura.com>
2015-03-23 13:55:18 -0700

    Changes to make it work in a hacky way

--- confd/bin/crappy-script.sh
+++ confd/bin/crappy-script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-while true; do
-  SERVER_LIST=$(grep "server" /etc/zookeeper/conf/zoo.cfg | grep -v "#")
-  for i in $(echo $SERVER_LIST); do
-    TEMP=$(echo $SERVER_LIST | awk -F= '{print $2}' | awk -F: '{print $1}')
-    if [ "$TEMP" == "$HOST_IP" ]
-      then
-      PRINT=$(echo $SERVER_LIST | awk -F. '{print $2}' | awk -F= '{print $1}')
-      echo $PRINT
-    fi
-  done
+SERVER_LIST=$(grep "server" /etc/zookeeper/conf/zoo.cfg | grep -v "#")
+for i in $(echo $SERVER_LIST); do
+  TEMP=$(echo $SERVER_LIST | awk -F= '{print $2}' | awk -F: '{print $1}')
+  if [ "$TEMP" == "$HOST_IP" ]
+    then
+    PRINT=$(echo $SERVER_LIST | awk -F. '{print $2}' | awk -F= '{print $1}')
+    echo "[crappy-start] changing myid to $PRINT"
+  fi
 done
+rm -f /etc/zookeeper/conf/myid
+touch /etc/zookeeper/conf/myid
+echo $PRINT > /etc/zookeeper/conf/myid
---
+++ confd/bin/crappy-start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/local/bin/crappy-script
+/usr/share/zookeeper/bin/zkServer.sh $1
