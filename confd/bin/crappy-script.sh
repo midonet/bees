2d117fb95f820bedc1942dd3200e54ba67881bee
timfallmk <tim@midokura.com>
2015-03-24 15:44:46 -0700

    Switching to while loop

--- confd/bin/crappy-script.sh
+++ confd/bin/crappy-script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 SERVER_LIST=$(grep "server" /etc/zookeeper/conf/zoo.cfg | grep -v "#")
-for i in $(echo $SERVER_LIST); do
+while read -r input; do
   TEMP=$(echo $SERVER_LIST | awk -F= '{print $2}' | awk -F: '{print $1}')
   if [ "$TEMP" == "$HOST_IP" ]
     then
     PRINT=$(echo $SERVER_LIST | awk -F. '{print $2}' | awk -F= '{print $1}')
     echo "[crappy-start] changing myid to $PRINT"
   fi
-done
+done <<< "$SERVER_LIST"
 rm -f /etc/zookeeper/conf/myid
 touch /etc/zookeeper/conf/myid
 echo $PRINT > /etc/zookeeper/conf/myid
