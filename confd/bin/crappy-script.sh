2128267464c24f8c3924079aa4e7784b68e2eed4
timfallmk <tim@midokura.com>
2015-03-24 16:04:01 -0700

    Forgot to substitute input

--- confd/bin/crappy-script.sh
+++ confd/bin/crappy-script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 SERVER_LIST=$(grep "server" /etc/zookeeper/conf/zoo.cfg | grep -v "#")
 while read -r input; do
-  TEMP=$(echo $SERVER_LIST | awk -F= '{print $2}' | awk -F: '{print $1}')
+  TEMP=$(echo $input | awk -F= '{print $2}' | awk -F: '{print $1}')
   if [ "$TEMP" == "$HOST_IP" ]
     then
-    PRINT=$(echo $SERVER_LIST | awk -F. '{print $2}' | awk -F= '{print $1}')
+    PRINT=$(echo $input | awk -F. '{print $2}' | awk -F= '{print $1}')
     echo "[crappy-start] changing myid to $PRINT"
   fi
 done <<< "$SERVER_LIST"
