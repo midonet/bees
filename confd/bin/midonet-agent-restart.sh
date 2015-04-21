9e3fb7a78ad7b19a86511ddd3cb0881fbc783dd1
timfallmk <tim@midokura.com>
2015-04-21 16:46:41 -0700

    Command substitution

--- confd/bin/midonet-agent-restart.sh
+++ confd/bin/midonet-agent-restart.sh
@@ -2,7 +2,7 @@
 $PIDFILE=/var/tmp/midonet-agent-pid.lock
 if [ -f $PIDFILE]; then
 	echo "[midonet-agent-monitor] Lockfile found at $PIDFILE, killing process"
-	$PID=${cat $PIDFILE}
+	$PID=$(cat $PIDFILE)
   kill $PID
   rm -f $PIDFILE
   echo "[midonet-agent-monitor] Killed process $PID, starting new process"
@@ -12,7 +12,7 @@ if [ -f $PIDFILE]; then
   echo "[midonet-agent-monitor] New process with PID $PID"
 else
   echo "[midonet-agent-monitor] No lockfile found, starting new process"
-  /usr/share/midolman/midolman-start
+  /usr/share/midolman/midolman-prepare && /usr/share/midolman/midolman-start
   $PID=$0
   echo $0 > $PIDFILE
   echo "[midonet-agent-monitor] New process with PID $PID"
