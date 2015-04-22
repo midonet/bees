ebed7dbf52359f713f2e6001461a3a1f53bf68f9
timfallmk <tim@midokura.com>
2015-04-21 17:38:00 -0700

    Rework restart script to fork and search

--- confd/bin/midonet-agent-restart.sh
+++ confd/bin/midonet-agent-restart.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 $PIDFILE=/var/tmp/midonet-agent-pid.lock
-if [ -f $PIDFILE]; then
+$WDPIDFILE=/var/tmp/midonet-agent-wd-pid.lock
+if [ -f $PIDFILE ]; then
 	echo "[midonet-agent-monitor] Lockfile found at $PIDFILE, killing process"
-	$PID=$(cat $PIDFILE)
+	PID=$(cat $PIDFILE)
+  WDPID=$(cat $PIDFILE)
   kill $PID
+  kill $WDPID
   rm -f $PIDFILE
+  rm -f $WDPIDFILE
   echo "[midonet-agent-monitor] Killed process $PID, starting new process"
-  /usr/share/midolman/midolman-start
-  $PID=$0
-  echo $0 > $PIDFILE
+  (/usr/share/midolman/midolman-start)
+  PID=$(pidof java)
+  WDPID=$(pidof wdog)
+  echo $PID > $PIDFILE
+  echo $WDPID > $WDPIDFILE
   echo "[midonet-agent-monitor] New process with PID $PID"
 else
   echo "[midonet-agent-monitor] No lockfile found, starting new process"
-  /usr/share/midolman/midolman-prepare && /usr/share/midolman/midolman-start
-  $PID=$0
-  echo $0 > $PIDFILE
+  (/usr/share/midolman/midolman-prepare && /usr/share/midolman/midolman-start)
+  PID=$(pidof java)
+  WDPID=$(pidof wdog)
+  echo $PID > $PIDFILE
+  echo $WDPID > $WDPIDFILE
   echo "[midonet-agent-monitor] New process with PID $PID"
 fi
