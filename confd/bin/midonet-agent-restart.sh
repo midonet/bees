68679ac039953c8157a0d67f553a5810d5dfee83
timfallmk <tim@midokura.com>
2015-04-23 19:28:30 -0700

    Use Phusion/baseimage

--- confd/bin/midonet-agent-restart.sh
+++ confd/bin/midonet-agent-restart.sh
@@ -10,7 +10,7 @@ if [ -f $PIDFILE ]; then
   rm -f $PIDFILE
   rm -f $WDPIDFILE
   echo "[midonet-agent-monitor] Killed process $PID, starting new process"
-  (/usr/share/midolman/midolman-start &)
+  sv restart midolman
   sleep 2
   PID=$(pidof java)
   WDPID=$(pidof wdog)
@@ -19,7 +19,7 @@ if [ -f $PIDFILE ]; then
   echo "[midonet-agent-monitor] New process with PID $PID"
 else
   echo "[midonet-agent-monitor] No lockfile found, starting new process"
-  (/usr/share/midolman/midolman-prepare && /usr/share/midolman/midolman-start &)
+  sv restart midolman
   sleep 2
   PID=$(pidof java)
   WDPID=$(pidof wdog)
